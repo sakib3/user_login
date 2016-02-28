458dd39639a203407adddbe72c3f8e3ec19033b0
sakib3 <sakib3@gmail.com>
2016-02-29 00:50:05 +0100

    seeding the users table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Model;
+use App\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -9,8 +10,36 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->UserTableSeeder();
+    }
+
+
+    /**
+     * Seeding user table.
+     *
+     * @return void
+     */
+    public function UserTableSeeder(){
+        //Temporarily disable the mass assignment protection of the model which helps to seed
+        Model::unguard();
+        //Empty users table
+        DB::table('users')->delete();
+
+        $user = array(
+                'name' => 'Sabbir Rahman Sakib',
+                'email' => 'sakib3@gmail.com',
+                'password' => Hash::make('secret'),
+
+        );
+
+        // create a user in the database
+        User::create($user);
+
+
+        Model::reguard();
     }
 }
