38ec6e162ee88ff21f9583ca0d5286c03de98ad1
sakib3 <sakib3@gmail.com>
2016-02-28 22:31:38 +0100

    add JWT and publish for auth

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //jwt-auth - handle authentication is with JSON Web Tokens
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +204,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        //jwt-auth facades
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
